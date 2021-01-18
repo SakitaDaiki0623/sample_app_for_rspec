6f4f927937b08c1ef13115d8ecde830b20b77a6f
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-01-18 10:57:07 +0900

    option_task02_model_spec;

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title) { |n| "test#{n} title" }
+    content { "example content" }
+    status { "todo" }
+    deadline { 1.week.from_now }
+    association :user, factory: :user
   end
 end
