6f546a21c9eab7156490fb051d57027be993ac95
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-01-22 11:20:31 +0900

    Fix:Userのシステムスペックファイルを回答に基づき修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,13 +5,10 @@ RSpec.describe 'Users', type: :system do
 
   let(:user) { create(:user, email: 'user@example.com') }
   let(:other_user) { create(:user, email: 'other_user@example.com') }
-  let!(:user_task) { create(:task, title: 'user_task', user: user) }
 
   describe 'before login' do
     describe 'sign up' do
-      before do
-        visit sign_up_path
-      end
+      before { visit sign_up_path }
       context 'when input values in the form are all valid' do
         before do
           fill_in 'Email', with: 'sample@example.com'
@@ -24,16 +21,21 @@ RSpec.describe 'Users', type: :system do
           expect(current_path).to eq login_path
         end
       end
+
       context 'when email is nil' do
         before do
+          fill_in 'Email', with: nil
           fill_in 'Password', with: 'password'
           fill_in 'Password confirmation', with: 'password'
           click_button 'SignUp'
         end
         it 'fails to create a new user' do
+          expect(page).to have_content '1 error prohibited this user from being saved'
           expect(page).to have_content "Email can't be blank"
+          expect(current_path).to eq users_path
         end
       end
+
       context 'when already registered email is used' do
         before do
           fill_in 'Email', with: user.email
@@ -42,16 +44,17 @@ RSpec.describe 'Users', type: :system do
           click_button 'SignUp'
         end
         it 'fails to create a new user' do
+          expect(page).to have_content '1 error prohibited this user from being saved'
           expect(page).to have_content 'Email has already been taken'
+          expect(current_path).to eq users_path
+          expect(page).to have_field 'Email', with: user.email
         end
       end
     end
     
     describe 'Mypage' do
       context 'when accesses to Mypage' do
-        before do
-          visit user_path(user)
-        end
+        before { visit user_path(user) }
         it 'fails to access' do
           expect(page).to have_content 'Login required'
           expect(current_path).to eq login_path
@@ -61,9 +64,7 @@ RSpec.describe 'Users', type: :system do
 
     describe 'new task page' do
       context 'when accesses to new task page' do
-        before do
-          visit new_task_path
-        end
+        before { visit new_task_path }
         it 'fails to access' do
           expect(page).to have_content 'Login required'
           expect(current_path).to eq login_path
@@ -74,6 +75,7 @@ RSpec.describe 'Users', type: :system do
     describe 'edit task page' do
       context 'when accesses to edit task page' do
         before do
+          user_task = create(:task, title: 'test_title', status: :doing, user: user)
           visit edit_task_path(user_task)
         end
         it 'fails to access' do
@@ -85,18 +87,14 @@ RSpec.describe 'Users', type: :system do
   end
   
   describe 'after user login' do
-    before do
-      sign_in_as(user)
-    end
+    before { sign_in_as(user) }
     describe 'edit user' do
-      before do
-        visit edit_user_path(user)
-      end
+      before { visit edit_user_path(user) }
       context 'when input values in the form are all valid' do
         before do
-          fill_in 'Email', with: 'sample@example.com'
-          fill_in 'Password', with: 'password'
-          fill_in 'Password confirmation', with: 'password'
+          fill_in 'Email', with: 'updated@example.com'
+          fill_in 'Password', with: 'updated_password'
+          fill_in 'Password confirmation', with: 'updated_password'
           click_button 'Update'
         end
         it 'is successfully updated' do
@@ -104,68 +102,58 @@ RSpec.describe 'Users', type: :system do
           expect(current_path).to eq user_path(user)
         end
       end
+
       context 'when email is nil' do
         before do
           fill_in 'Email', with: nil
-          fill_in 'Password', with: 'password'
-          fill_in 'Password confirmation', with: 'password'
+          fill_in 'Password', with: 'updated_password'
+          fill_in 'Password confirmation', with: 'updated_password'
           click_button 'Update'
         end
         it 'fails to be editted' do
+          expect(page).to have_content '1 error prohibited this user from being saved'
           expect(page).to have_content "Email can't be blank"
+          expect(current_path).to eq user_path(user)
         end
       end
+
       context 'when already registered email is used' do
         before do
           fill_in 'Email', with: other_user.email
-          fill_in 'Password', with: 'password'
-          fill_in 'Password confirmation', with: 'password'
+          fill_in 'Password', with: 'updated_password'
+          fill_in 'Password confirmation', with: 'updated_password'
           click_button 'Update'
         end
         it 'fails to be editted' do
+          expect(page).to have_content '1 error prohibited this user from being saved'
           expect(page).to have_content 'Email has already been taken'
+          expect(current_path).to eq user_path(user)
+        end
+      end
+
+      context 'when user accesses to page to edit other_user' do
+        before { visit edit_user_path(other_user) }
+        it 'fails to access' do
+          expect(page).to have_content 'Forbidden access.'
+          expect(current_path).to eq user_path(user)
         end
       end
     end
 
     describe 'Mypage' do
-      context 'user accesses to Mypage' do
+      context 'when user creates a new task' do
         before do
+          create(:task, title: 'test_title', status: :doing, user: user)
           visit user_path(user)
         end
-        it 'user email, user_task, links of user_task are displayed are all desplayed' do
-          expect(page).to have_content user.email
-          expect(page).to have_content 'You have 1 task.'
-          expect(page).to have_content user_task.title
-          expect(page).to have_content user_task.status
-          expect(page).to have_link 'Show', href: task_path(user_task)
-          expect(page).to have_link 'Edit', href: edit_task_path(user_task)
-          expect(page).to have_link 'Destroy', href: task_path(user_task)
+        it 'the new task is displayed' do
+          expect(page).to have_content('You have 1 task.')
+          expect(page).to have_content('test_title')
+          expect(page).to have_content('doing')
+          expect(page).to have_link('Show')
+          expect(page).to have_link('Edit')
+          expect(page).to have_link('Destroy')
         end
-      end
-    end
-  end
-
-  describe 'after other_user login' do
-    before do
-      sign_in_as(other_user)
-    end
-    context 'when other_user accesses to edit user page' do
-      before do
-        visit edit_user_path(user)
-      end
-      it 'fails to access' do
-        expect(page).to have_content 'Forbidden access.'
-        expect(current_path).to eq user_path(other_user)
-      end
-    end
-    context "when other_user accesses to user's task edit page" do
-      before do
-        visit edit_task_path(user_task)
-      end
-      it 'fails to access' do
-        expect(page).to have_content 'Forbidden access.'
-        expect(current_path).to eq root_path
       end
     end
   end
